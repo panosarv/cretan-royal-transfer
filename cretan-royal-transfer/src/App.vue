8e36df0f6445a3cd69de536e009d5cf44f4f82d6
Panagiotis Arvanitis <97358977+panosarv@users.noreply.github.com>
2026-04-21 02:22:01 +0300

    feat: add page route transitions to App.vue

--- cretan-royal-transfer/src/App.vue
+++ cretan-royal-transfer/src/App.vue
@@ -1,13 +1,16 @@
 <script setup>
 import Navbar from './components/Navbar.vue'
-import ContactSection from './components/ContactSection.vue';
-
+import ContactSection from './components/ContactSection.vue'
 </script>
 
 <template>
-  <div class="min-h-screen bg-stone-50">
+  <div class="min-h-screen bg-brand-stone">
     <Navbar />
-    <router-view />
+    <RouterView v-slot="{ Component }">
+      <Transition name="page" mode="out-in">
+        <component :is="Component" :key="$route.path" />
+      </Transition>
+    </RouterView>
     <ContactSection />
   </div>
 </template>
