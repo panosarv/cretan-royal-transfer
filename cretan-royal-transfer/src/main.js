fb77497500f2c7bd2bc345156bdddbe86bc5a2bf
Panagiotis Arvanitis <97358977+panosarv@users.noreply.github.com>
2026-04-21 01:07:15 +0300

    feat: install and register Pinia

--- cretan-royal-transfer/src/main.js
+++ cretan-royal-transfer/src/main.js
@@ -1,11 +1,12 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import App from './App.vue';
-import router from './router'; // Import router
+import router from './router';
 import i18n from './i18n';
-
 import './style.css';
 
 const app = createApp(App);
-app.use(router); // Use the router
+app.use(createPinia());
+app.use(router);
 app.use(i18n);
 app.mount('#app');
